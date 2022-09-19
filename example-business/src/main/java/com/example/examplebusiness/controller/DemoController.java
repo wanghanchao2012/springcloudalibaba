065712beacd22206fef97c82845f1f825f0e1a4a
wanghanchao2012 <35331347+wanghanchao2012@users.noreply.github.com>
2022-09-19 13:27:28 +0800

    code change

--- example-business/src/main/java/com/example/examplebusiness/controller/DemoController.java
+++ example-business/src/main/java/com/example/examplebusiness/controller/DemoController.java
@@ -5,11 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.support.StandardServletEnvironment;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
@@ -27,6 +32,24 @@ public class DemoController {
         String property = en.getProperty("mytest.name1");
         System.out.println("是的，感觉不错！！！！！！！");
         return JsonResult.success("current server port:" + port + ",config name:" + name);
+    }
+
+    @RequestMapping("/env")
+    public String env() {
+        StandardServletEnvironment environment = (StandardServletEnvironment) en;
+        MutablePropertySources propertySources = environment.getPropertySources();
+        Iterator<PropertySource<?>> iterator = propertySources.iterator();
+        while (iterator.hasNext()) {
+            PropertySource<?> propertySource = iterator.next();
+            if (propertySource instanceof MapPropertySource) {
+                MapPropertySource mapPropertySource = (MapPropertySource) propertySource;
+                String[] propertyNames = mapPropertySource.getPropertyNames();
+                for (String s : propertyNames) {
+                    System.out.println(s + ":" + mapPropertySource.getProperty(s));
+                }
+            }
+        }
+        return en.getProperty("user.name") + "-" + en.getProperty("user.age");
     }
 
     @GetMapping("/getlist")
